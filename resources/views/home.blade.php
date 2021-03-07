5e26daff1010fb92c11ed9e1efe7c56ec61216f1
keisuke-1-7 <keisuke17.business@gmail.com>
2021-03-07 11:38:01 +0900

    ログイン後home画面レスポンシブ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
     <main class="container position-absolute" style="top: 50%;left: 50%;transform: translate(-50%, -50%);">
         <div class="" >
             <div class="row">
-                <div class="col-md-6 text-center">
-                    <img class="home_logo" src="{{ asset('/meet_images/meet_logo.png') }}" alt="">
+                <div class="home-left col-md-6 col-sm-12 col-xs-12 text-center">
+                    <img class="home_logo col-md-8 col-sm-7 col-xs-5 p-0" src="{{ asset('/meet_images/meet_logo.png') }}" alt="">
                 </div>
-                <div class="home-right col-md-6">
+                <div class="home-right col-md-6 col-sm-12 col-xs-12">
                     <h3>ご利用ありがとうございます</h3>
                     <h3 class="mt-5">今食べたいお肉と出会いましょう</h3>
                     <div class="text-center">
