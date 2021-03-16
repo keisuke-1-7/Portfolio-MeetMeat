d1ffea377ecfb0197749298a5892f420ddcc3757
keisuke-1-7 <keisuke17.business@gmail.com>
2021-03-16 12:52:33 +0900

    ハンバーガーメニューの作成

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -5,7 +5,7 @@
     </h1>
     @if (Auth::check())
         <p class="header-user-name">{{ !empty(Auth::user()) ? Auth::user()->last_name . Auth::user()->first_name : 'ユーザー' }} さん</p>
-        <nav>
+        <nav class="normal-menu">
             <ul>
                 <li class="nav-item">{!! link_to_route('show', '商品検索', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('cartlist', 'カート', [], ['class' => 'nav-link']) !!}</li>
@@ -13,6 +13,19 @@
                 <li class="nav-item">{!! link_to_route('user_info', 'ユーザ情報', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
         </nav>
+        <div class="hamburger-menu">
+            <input type="checkbox" id="menu-btn-check">
+            <label for="menu-btn-check" class="menu-btn"><span></span></label>
+            <div class="menu-content">
+                <ul class="column">
+                    <li class="nav-item">{!! link_to_route('show', '商品検索', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('cartlist', 'カート', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('o_history', '注文履歴', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('user_info', 'ユーザ情報', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
+                </ul>
+            </div>
+        </div>
     @else
         <nav class="pc-nav">
             <ul>
